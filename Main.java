d094211c5beab892508e45e498021676d95765ad
andinidwi14 <andinidwistudy@gmail.com>
2026-05-06 16:17:59 +0700

    menambahkan pajak custom

--- Main.java
+++ Main.java
@@ -16,11 +16,19 @@ public class Main {
     System.out.println("====== RINCIAN PAJAK ELEKTRONIK ======");
 
     // Versi 1 - pajak standar
-        double pajakLaptop = Laptop.hitungPajak(1);
-        double totalLaptop = Laptop.hitungHargaSetelahDiskon(1);
-        System.out.println("Laptop x1 (PPN standar 11%)");
-        System.out.println("  Subtotal : Rp" + Laptop.hitungTotalHarga(1));
-        System.out.println("  Pajak    : Rp" + pajakLaptop);
-        System.out.println("  Total    : Rp" + totalLaptop)
+    double pajakLaptop = Laptop.hitungPajak(1);
+    double totalLaptop = Laptop.hitungHargaSetelahDiskon(1);
+    System.out.println("Laptop x1 (PPN standar 11%)");
+    System.out.println("  Subtotal : Rp" + Laptop.hitungTotalHarga(1));
+    System.out.println("  Pajak    : Rp" + pajakLaptop);
+    System.out.println("  Total    : Rp" + totalLaptop);
+
+    // Versi 2 - pajak custom 5% (misal barang impor khusus)
+    double pajakHpCustom = hp.hitungPajak(2);
+    System.out.println("HP Samsung x2 (pajak custom 5%)");
+    System.out.println("  Subtotal : Rp" + hp.hitungTotalHarga(2));
+    System.out.println("  Pajak    : Rp" + pajakHpCustom);
+    System.out.println("  Total    : Rp" + (hp.hitungTotalHarga(2) + pajakHpCustom));
+
     }
 }
